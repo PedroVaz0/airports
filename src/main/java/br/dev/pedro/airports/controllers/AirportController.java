955130ebdd2b7f687b5a3de483fe8bc21f168e02
Pedro Henrique Vaz Almeida <ph7519490@gmail.com>
2025-11-27 13:51:02 -0300

    Termino da Fase 2

---
+++ src/main/java/br/dev/pedro/airports/controllers/AirportController.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.dev.pedro.airports.controllers;
+
+import br.dev.pedro.airports.entities.Airport;
+import br.dev.pedro.airports.service.AirportService;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+
+
+@RestController
+public class AirportController {
+
+    @Autowired
+    private AirportService airportService;
+
+    @GetMapping("/airport")
+    public List<Airport> findAll() {
+        List<Airport> result = airportService.findAll();
+        return result;
+    }
+    
+}
---
+++ src/main/java/br/dev/pedro/airports/repositories/AirportRepository.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Interface.java to edit this template
+ */
+package br.dev.pedro.airports.repositories;
+
+import br.dev.pedro.airports.entities.Airport;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+/**
+ *
+ * @author DIT2B
+ */
+public interface AirportRepository extends JpaRepository<Airport, Long> {
+    
+}
